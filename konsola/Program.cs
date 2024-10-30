4ceddb9ec6d611edd052cd9c65bad582f8af4b16
Bartłomiej Więcek <bartlomiej.wiecek@vp.pl>
2024-10-30 09:53:48 +0100

    separated code into functions

--- konsola/Program.cs
+++ konsola/Program.cs
@@ -2,6 +2,35 @@
 {
     internal class Program
     {
+        static List<int> DrawDices(int n)
+        {
+            Random random = new Random();
+            List<int> draws = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                draws.Add(random.Next(1, 7));
+            }
+            return draws;
+        }
+
+        static int CountPoints(List<int> draws)
+        {
+            int points = 0;
+            List<int> numberOfDraws = new List<int>() { 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < draws.Count; i++)
+            {
+                numberOfDraws[draws[i] - 1]++;
+            }
+
+            for (int i = 0; i < numberOfDraws.Count; i++)
+            {
+                if (numberOfDraws[i] > 1)
+                    points += (i + 1) * numberOfDraws[i];
+            }
+
+            return points;
+        }
+
         static void Main(string[] args)
         {
             int n = 0;
@@ -15,21 +44,12 @@
                     n = int.Parse(Console.ReadLine()!);
                 } while (n < 3 || n > 10);
 
-                List<int> throws = new List<int>() { 0, 0, 0, 0, 0, 0 };
-                Random rand = new Random();
-                for(int i = 1; i <= n; i++)
+                List<int> draws = DrawDices(n);
+                for(int i = 0; i < draws.Count; i++)
                 {
-                    int diceIndex = rand.Next(6);
-                    throws[diceIndex]++;
-                    Console.WriteLine("Kostka " + i + ": " + (diceIndex + 1));
+                    Console.WriteLine("Kostka " + (i + 1) + ": " + draws[i]);
                 }
-
-                int points = 0;
-                for(int i = 0; i < throws.Count; i++)
-                {
-                    if (throws[i] > 1)
-                        points += (i + 1) * throws[i];
-                }
+                int points = CountPoints(draws);
                 Console.WriteLine("Liczba uzyskanych punktów: " + points);
 
                 Console.WriteLine("Jeszcze raz? (t/n)");
